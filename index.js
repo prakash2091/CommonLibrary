ee7e879211ecff798b0936563e57aeb0600dc831
Prakash <prakash.subramanian@yara.com>
2022-05-30 08:35:58 +0800

    updated index

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// AppRegistry.registerComponent(appName, () => App);
+
+import Card from './lib/src/Components/Card';
+import NewCard from './lib/src/Components/NewCard';
+
+export {Card as default, NewCard};
