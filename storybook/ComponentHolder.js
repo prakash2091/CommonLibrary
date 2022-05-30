636192c3e5968b8aad43db3374a982b203195c0c
Prakash <prakash.subramanian@yara.com>
2022-05-30 08:15:53 +0800

    first commit

---
+++ storybook/ComponentHolder.js
@@ -0,0 +1,10 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+const ComponentHolder = () => {
+  require('../lib/src/Components/Card');
+};
+
+export default ComponentHolder;
